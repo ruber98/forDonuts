4826dafeb5d018ba9d9baba2f6170be40e670bc1
Adam Driggers <awdriggs@gmail.com>
2016-03-15 13:53:29 -0400

    donuts

---
+++ for.js
@@ -0,0 +1,8 @@
+
+
+//fill the screen with 100 donuts.
+// whole donut "http://extras.mnginteractive.com/live/media/site36/2007/0724/20070724_070849_fd25doughnut_300.jpg")
+
+//bonus, when a donut is clicked, change the image to the donut with bite 
+// "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_DmEip_O7BcvEO51FoT8z4zWCnTiWgTvABE6wc5gDtLpLvqND"
+
